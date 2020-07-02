36b3704de71309f09c6283b9fe31ce555c059b70
Swain Zheng <swain.zheng@ringcentral.com>
2020-07-02 15:36:52 +0800

    chore: recover adb port forward

--- src/port_forward.sh
+++ src/port_forward.sh
@@ -2,5 +2,7 @@
 
 #Ubuntu 16.04 -> ip=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 ip=$(ifconfig eth0 | grep 'inet' | cut -d: -f2 | awk '{ print $2}')
-socat tcp-listen:5554,bind=$ip,fork tcp:127.0.0.1:5554 &
-socat tcp-listen:5555,bind=$ip,fork tcp:127.0.0.1:5555
+adb_port=$ADB_PORT
+adb_console_port=$ADB_PORT-1
+socat tcp-listen:$adb_console_port,bind=$ip,fork tcp:127.0.0.1:$adb_console_port &
+socat tcp-listen:$adb_port,bind=$ip,fork tcp:127.0.0.1:$adb_port
