3ae539e884709a68cc430614b5827ef548a3487a
Swain Zheng <swain.zheng@ringcentral.com>
2020-08-04 13:22:58 +0800

    chore: emulator add timezone shanghai

--- src/app.py
+++ src/app.py
@@ -223,11 +223,11 @@ def run():
     if is_first_run:
         logger.info('Emulator was not previously initialized. Preparing a new one...')
         port = os.getenv('UDID').replace('emulator-','')
-        cmd = 'emulator/emulator @{name} -port {port} -gpu swiftshader_indirect -accel on -wipe-data -writable-system -verbose {custom_args}'.format(name=avd_name, port=port, custom_args=custom_args)
+        cmd = 'emulator/emulator @{name} -port {port} -timezone Asia/Shanghai -gpu swiftshader_indirect -accel on -wipe-data -writable-system -verbose {custom_args}'.format(name=avd_name, port=port, custom_args=custom_args)
     else:
         logger.info('Using previously initialized AVD...')
         port = os.getenv('UDID').replace('emulator-','')
-        cmd = 'emulator/emulator @{name} -port {port} -gpu swiftshader_indirect -accel on -verbose -writable-system {custom_args}'.format(name=avd_name, port=port, custom_args=custom_args)
+        cmd = 'emulator/emulator @{name} -port {port} -timezone Asia/Shanghai -gpu swiftshader_indirect -accel on -verbose -writable-system {custom_args}'.format(name=avd_name, port=port, custom_args=custom_args)
 
     appium = convert_str_to_bool(str(os.getenv('APPIUM', False)))
     if appium:
